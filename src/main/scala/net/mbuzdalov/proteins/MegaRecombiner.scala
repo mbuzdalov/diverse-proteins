ada150e847810d891186d0b2171c269ff77b5710
Maxim Buzdalov <mbuzdalov@gmail.com>
2024-09-17 17:34:18 +0100

    Rewrite the recombiner with bitsets.

--- src/main/scala/net/mbuzdalov/proteins/MegaRecombiner.scala
+++ src/main/scala/net/mbuzdalov/proteins/MegaRecombiner.scala
@@ -1,11 +1,12 @@
 package net.mbuzdalov.proteins
 
+import scala.collection.mutable.{BitSet => MuBitSet}
+
 object MegaRecombiner:
   def recombine(cont: Container, count: Int, proteins: String): IArray[Int] =
     val indices = proteins.split(',').map(cont.index).distinct
     println(s"Number of proteins to consider: ${indices.length}")
     println(s"    Proteins: ${indices.map(cont.name).mkString(",")}")
-    require(indices.length < 64)
     require(indices.length > count)
 
     val t0 = System.nanoTime()
@@ -15,25 +16,25 @@ object MegaRecombiner:
 
     val recombiner = new MegaRecombiner(matrix, count)
     val t1 = System.nanoTime()
-    recombiner.go(0, 0L, Double.PositiveInfinity, 0, Array.fill(indices.length)(Double.PositiveInfinity))
+    recombiner.go(0, new MuBitSet(), Double.PositiveInfinity, 0, Array.fill(indices.length)(Double.PositiveInfinity))
     println(f"    Time spent in brute-force: ${(System.nanoTime() - t1) * 1e-9}%.02f seconds")
-    val solution = indices.indices.filter(i => (recombiner.currentBestMask & (1L << i)) != 0).map(indices)
+    val solution = indices.indices.filter(i => recombiner.currentBestMask(i)).map(indices)
     val checkup = cont.evaluateFromScratch(solution *)
     assert(checkup == recombiner.bestMin)
     IArray(solution *)
 
 class MegaRecombiner private(m: IArray[IArray[Double]], count: Int):
   private var bestMin, bestMinSum = 0.0
-  private var currentBestMask = 0L
-  def go(index: Int, mask: Long, prefixMin: Double, prefixMinSum: Double, prefixMinima: Array[Double]): Unit =
+  private var currentBestMask = new MuBitSet()
+  def go(index: Int, mask: MuBitSet, prefixMin: Double, prefixMinSum: Double, prefixMinima: Array[Double]): Unit =
     //assert(prefixMin > bestMin || prefixMin == bestMin && prefixMinSum > bestMinSum)
     assert(prefixMin >= bestMin)
-    val nBits = java.lang.Long.bitCount(mask)
+    val nBits = mask.size
     if nBits == count then
       if prefixMin > bestMin || prefixMin == bestMin && prefixMinSum > bestMinSum then
         bestMin = prefixMin
         bestMinSum = prefixMinSum
-        currentBestMask = mask
+        currentBestMask = mask.clone()
         println(s"Updated to ($bestMin, $bestMinSum) by $currentBestMask")
     else
       assert(index < m.length)
@@ -44,7 +45,7 @@ class MegaRecombiner private(m: IArray[IArray[Double]], count: Int):
         var updatedMin = Double.PositiveInfinity
         var updatedMinSum = 0.0
         Loops.foreach(0, index): i =>
-          if (mask & (1L << i)) != 0 then
+          if mask(i) then
             val v = m(i)(index)
             updatedMin = math.min(updatedMin, v)
             updatedMinSum += math.min(prefixMinima(i), v)
@@ -53,8 +54,10 @@ class MegaRecombiner private(m: IArray[IArray[Double]], count: Int):
         if updatedMin >= bestMin then
           val copy = prefixMinima.clone()
           Loops.foreach(0, index): i =>
-            if (mask & (1L << i)) != 0 then
+            if mask(i) then
               val d = m(i)(index)
               copy(i) = math.min(copy(i), d)
               copy(index) = math.min(copy(index), d)
-          go(index + 1, mask | (1L << index), math.min(prefixMin, updatedMin), updatedMinSum, copy)
+          mask.addOne(index)
+          go(index + 1, mask, math.min(prefixMin, updatedMin), updatedMinSum, copy)
+          mask.remove(index)
