6847a0f4c587178f2c0c70017e66d3e66a3d7206
Maxim Buzdalov <mbuzdalov@gmail.com>
2024-09-17 22:23:33 +0100

    Save some more RAM in the recombiner.

--- src/main/scala/net/mbuzdalov/proteins/MegaRecombiner.scala
+++ src/main/scala/net/mbuzdalov/proteins/MegaRecombiner.scala
@@ -16,7 +16,7 @@ object MegaRecombiner:
 
     val recombiner = new MegaRecombiner(matrix, count)
     val t1 = System.nanoTime()
-    recombiner.go(0, new MuBitSet(), Double.PositiveInfinity, 0, Array.fill(indices.length)(Double.PositiveInfinity))
+    recombiner.go(0, Double.PositiveInfinity, 0, 0)
     println(f"    Time spent in brute-force: ${(System.nanoTime() - t1) * 1e-9}%.02f seconds")
     val solution = indices.indices.filter(i => recombiner.currentBestMask(i)).map(indices)
     val checkup = cont.evaluateFromScratch(solution *)
@@ -26,38 +26,43 @@ object MegaRecombiner:
 class MegaRecombiner private(m: IArray[IArray[Double]], count: Int):
   private var bestMin, bestMinSum = 0.0
   private var currentBestMask = new MuBitSet()
-  def go(index: Int, mask: MuBitSet, prefixMin: Double, prefixMinSum: Double, prefixMinima: Array[Double]): Unit =
+  private val currMask = new MuBitSet()
+  private val depthStack = Array.fill(m.length, m.length + 1)(Double.PositiveInfinity)
+  
+  def go(index: Int, prefixMin: Double, prefixMinSum: Double, depth: Int): Unit =
     //assert(prefixMin > bestMin || prefixMin == bestMin && prefixMinSum > bestMinSum)
+    val prefixMinima = depthStack(depth)
     assert(prefixMin >= bestMin)
-    val nBits = mask.size
+    val nBits = currMask.size
     if nBits == count then
       if prefixMin > bestMin || prefixMin == bestMin && prefixMinSum > bestMinSum then
         bestMin = prefixMin
         bestMinSum = prefixMinSum
-        currentBestMask = mask.clone()
+        currentBestMask = currMask.clone()
         println(s"Updated to ($bestMin, $bestMinSum) by $currentBestMask")
     else
       assert(index < m.length)
       if m.length - index - 1 >= count - nBits then
-        go(index + 1, mask, prefixMin, prefixMinSum, prefixMinima)
+        go(index + 1, prefixMin, prefixMinSum, depth)
       //if prefixMin > bestMin || prefixMin == bestMin && prefixMinSum > bestMinSum then
       if prefixMin >= bestMin then
         var updatedMin = Double.PositiveInfinity
         var updatedMinSum = 0.0
         Loops.foreach(0, index): i =>
-          if mask(i) then
+          if currMask(i) then
             val v = m(i)(index)
             updatedMin = math.min(updatedMin, v)
             updatedMinSum += math.min(prefixMinima(i), v)
         updatedMinSum += updatedMin // the row that comes with the index being added
         //if updatedMin > bestMin || updatedMin == bestMin && updatedMinSum > bestMinSum then
         if updatedMin >= bestMin then
-          val copy = prefixMinima.clone()
+          val next = depthStack(depth + 1)
+          System.arraycopy(prefixMinima, 0, next, 0, prefixMinima.length)
           Loops.foreach(0, index): i =>
-            if mask(i) then
+            if currMask(i) then
               val d = m(i)(index)
-              copy(i) = math.min(copy(i), d)
-              copy(index) = math.min(copy(index), d)
-          mask.addOne(index)
-          go(index + 1, mask, math.min(prefixMin, updatedMin), updatedMinSum, copy)
-          mask.remove(index)
+              next(i) = math.min(next(i), d)
+              next(index) = math.min(next(index), d)
+          currMask.addOne(index)
+          go(index + 1, math.min(prefixMin, updatedMin), updatedMinSum, depth + 1)
+          currMask.remove(index)
