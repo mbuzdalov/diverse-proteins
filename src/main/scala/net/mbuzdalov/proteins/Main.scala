83770cf1f19946f7ee1ccd8ef8da53b35ff5b161
Maxim Buzdalov <mbuzdalov@gmail.com>
2024-09-17 22:23:58 +0100

    Support running on subsets of the big protein set.

--- src/main/scala/net/mbuzdalov/proteins/Main.scala
+++ src/main/scala/net/mbuzdalov/proteins/Main.scala
@@ -11,16 +11,31 @@ import scala.util.Using
 
 object Main:
   @tailrec
-  private def readEmbeddingsForever(source: ObjectInputStream, target: mutable.Growable[(String, IArray[Float])]): Nothing =
-    target += (source.readUTF() -> source.readObject().asInstanceOf[IArray[Float]])
-    readEmbeddingsForever(source, target)
+  private def readEmbeddingsForever(source: ObjectInputStream,
+                                    target: mutable.Growable[(String, IArray[Float])],
+                                    filter: String => Boolean): Nothing =
+    val key = source.readUTF()
+    val value = source.readObject().asInstanceOf[IArray[Float]]
+    if filter(key) then target += (key -> value)
+    readEmbeddingsForever(source, target, filter)
 
-  private def readEmbeddings(source: String): Container =
+  private def readEmbeddings(source: String, filterFile: Option[String]): Container =
     val startTime = System.nanoTime()
+    val filterFun = filterFile match
+      case None => (name: String) => true
+      case Some(f) => Using.resource(new BufferedReader(new FileReader(f))): br =>
+        val setB = Set.newBuilder[String]
+        var line = br.readLine()
+        while line != null do
+          setB += line
+          line = br.readLine()
+        val set = setB.result()
+        set.contains
+
     Using.resource(new ObjectInputStream(new GZIPInputStream(new FileInputStream(source)))): in =>
       val builder = IArray.newBuilder[(String, IArray[Float])]
       try
-        readEmbeddingsForever(in, builder)
+        readEmbeddingsForever(in, builder, filterFun)
       catch
         case e: EOFException =>
           println("# Embeddings loaded in %.01f seconds".formatLocal(Locale.US, (System.nanoTime() - startTime) * 1e-9))
@@ -45,15 +60,21 @@ object Main:
   private def printTimeSpentSince(timeNanos: Long): Unit =
     println("# Time spent: %.01f seconds".formatLocal(Locale.US, (System.nanoTime() - timeNanos) * 1e-9))
 
+  private def readEmbeddingsWrapper(args: Array[String]): Container =
+    val mainFile = args(1)
+    val indexOfSubset = args.indexOf("--subset")
+    val subsetFile = if indexOfSubset < 0 then None else Some(args(indexOfSubset + 1))
+    readEmbeddings(mainFile, subsetFile)
+  
   def main(args: Array[String]): Unit =
     args(0) match
       case "import" => Importing.importEmbeddings(args(1), args(2))
       case "stats" =>
-        val data = readEmbeddings(args(1))
+        val data = readEmbeddingsWrapper(args)
         println(s"${data.size} proteins")
       case "greedy" =>
         val count = args(2).toInt
-        val data = readEmbeddings(args(1))
+        val data = readEmbeddingsWrapper(args)
         runParallel(100):
           val t0 = System.nanoTime()
           val (solution, first) = Greedy.run(data, count)
@@ -62,12 +83,12 @@ object Main:
             println(data.explainSolution(solution))
       case "measure" =>
         val sets = args.drop(2)
-        val data = readEmbeddings(args(1))
+        val data = readEmbeddingsWrapper(args)
         evaluate(data, sets)
       case "local-rnd" =>
         val useMin = args(3) == "min"
         val count = args(2).toInt
-        val data = readEmbeddings(args(1))
+        val data = readEmbeddingsWrapper(args)
         runParallel(100):
           val t0 = System.nanoTime()
           val solution = LocalSearch.optimize(data, count, useMin)
@@ -77,7 +98,7 @@ object Main:
       case "local-greedy" =>
         val useMin = args(3) == "min"
         val count = args(2).toInt
-        val data = readEmbeddings(args(1))
+        val data = readEmbeddingsWrapper(args)
         runParallel(100):
           val t0 = System.nanoTime()
           val (initial, _) = Greedy.run(data, count)
@@ -88,11 +109,11 @@ object Main:
       case "recombine" =>
         val count = args(2).toInt
         val proteins = args.drop(3).mkString(",").replace(",,", ",")
-        val data = readEmbeddings(args(1))
+        val data = readEmbeddingsWrapper(args)
         val solution = MegaRecombiner.recombine(data, count, proteins)
         println(data.explainSolution(solution))
       case "heatmap" =>
-        val data = readEmbeddings(args(1))
+        val data = readEmbeddingsWrapper(args)
         for a <- args.drop(2) do
           val eq = a.indexOf('=')
           val filename = a.take(eq)
